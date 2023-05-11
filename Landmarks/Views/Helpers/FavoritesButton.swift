87cebd7f47addb077dcd1fd6732644f62463b7b3
Leo Villalobos <villaleobos@gmail.com>
2023-05-10 22:19:45 -0700

    Create FavoritesButton.swift

---
+++ Landmarks/Views/Helpers/FavoritesButton.swift
@@ -0,0 +1,28 @@
+//
+//  FavoritesButton.swift
+//  Landmarks
+//
+//  Created by Leonardo Villalobos on 5/10/23.
+//
+
+import SwiftUI
+
+struct FavoritesButton: View {
+  @Binding var isSet: Bool
+  
+    var body: some View {
+      Button {
+        isSet.toggle()
+      } label: {
+        Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+          .labelStyle(.iconOnly)
+          .foregroundColor(isSet ? .yellow : .gray)
+      }
+    }
+}
+
+struct FavoritesButton_Previews: PreviewProvider {
+    static var previews: some View {
+      FavoritesButton(isSet: .constant(true))
+    }
+}
