183ceadbceafe91819babb84c8edf5d406d1fca7
Leo Villalobos <villaleobos@gmail.com>
2023-05-10 17:07:00 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Leonardo Villalobos on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
