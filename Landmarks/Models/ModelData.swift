2a7eb51b672eecf953a8409ad479779229a0e196
Leo Villalobos <villaleobos@gmail.com>
2023-05-11 12:50:21 -0700

    Add category field to Landmark.swift

--- Landmarks/Models/ModelData.swift
+++ Landmarks/Models/ModelData.swift
@@ -10,7 +10,11 @@ import Combine
 
 final class ModelData: ObservableObject {
   @Published var landmarks: [Landmark] = load("landmarkData.json")
+  
   var hikes: [Hike] = load("hikeData.json")
+  var categories: [String: [Landmark]] {
+    Dictionary(grouping: landmarks, by: { $0.category.rawValue })
+  }
 }
 
 func load<T>(_ filename: String) -> T
