820c6202628be19f4dd9931c45069ac64fcd7bad
Leo Villalobos <villaleobos@gmail.com>
2023-05-11 08:32:57 -0700

    Create BadgeBackground.swift

--- Landmarks/Views/BadgeBackground.swift
+++ Landmarks/Views/BadgeBackground.swift
@@ -5,16 +5,57 @@
 //  Created by Leonardo Villalobos on 5/11/23.
 //
 
+import CoreGraphics
 import SwiftUI
 
 struct BadgeBackground: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var body: some View {
+    GeometryReader { geometry in
+      Path { path in
+        var width = min(geometry.size.width, geometry.size.height)
+        let height = width
+        
+        let xScale: CGFloat = 0.832
+        let xOffset = (width * (1.0 - xScale)) / 2.0
+        width *= xScale
+        
+        path.move(to: CGPoint(
+          x: width * 0.95 + xOffset,
+          y: height * (0.2 + HexagonParameters.adjustment)
+        ))
+        
+        HexagonParameters.segments.forEach { segment in
+          path.addLine(to: CGPoint(
+            x: width * segment.line.x + xOffset,
+            y: height * segment.line.y
+          ))
+          path.addQuadCurve(
+            to: CGPoint(
+              x: width * segment.curve.x + xOffset,
+              y: height * segment.curve.y
+            ),
+            control: CGPoint(
+              x: width * segment.control.x + xOffset,
+              y: height * segment.control.y
+            )
+          )
+        }
+      }
+      .fill(.linearGradient(
+        Gradient(colors: [Self.gradientStart, Self.gradientEnd]),
+        startPoint: UnitPoint(x: 0.5, y: 0),
+        endPoint: UnitPoint(x: 0.5, y: 0.6))
+      )
+      .aspectRatio(1, contentMode: .fit)
     }
+  }
+  
+  static let gradientStart = Color(red: 269.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
+  static let gradientEnd = Color(red: 240.0 / 255, green: 225.0 / 255, blue: 120.0 / 255)
 }
 
 struct BadgeBackground_Previews: PreviewProvider {
-    static var previews: some View {
-        BadgeBackground()
-    }
+  static var previews: some View {
+    BadgeBackground()
+  }
 }
--- Landmarks/Views/HexagonParameters.swift
+++ Landmarks/Views/HexagonParameters.swift
@@ -5,4 +5,48 @@
 //  Created by Leonardo Villalobos on 5/11/23.
 //
 
+import CoreGraphics
 import Foundation
+
+struct HexagonParameters {
+  struct Segment {
+    let line:    CGPoint
+    let curve:   CGPoint
+    let control: CGPoint
+  }
+  
+  static let adjustment: CGFloat = 0.085
+  
+  static let segments: [Segment] = [
+    .init(
+      line:    CGPoint(x: 0.60, y: 0.05),
+      curve:   CGPoint(x: 0.40, y: 0.05),
+      control: CGPoint(x: 0.50, y: 0.00)
+    ),
+    .init(
+      line:    CGPoint(x: 0.05, y: 0.20 + adjustment),
+      curve:   CGPoint(x: 0.00, y: 0.30 + adjustment),
+      control: CGPoint(x: 0.00, y: 0.25 + adjustment)
+    ),
+    .init(
+      line:    CGPoint(x: 0.00, y: 0.70 - adjustment),
+      curve:   CGPoint(x: 0.05, y: 0.80 - adjustment),
+      control: CGPoint(x: 0.00, y: 0.75 - adjustment)
+    ),
+    .init(
+      line:    CGPoint(x: 0.40, y: 0.95),
+      curve:   CGPoint(x: 0.60, y: 0.95),
+      control: CGPoint(x: 0.50, y: 1.00)
+    ),
+      .init(
+      line:    CGPoint(x: 0.95, y: 0.80 - adjustment),
+      curve:   CGPoint(x: 1.00, y: 0.70 - adjustment),
+      control: CGPoint(x: 1.00, y: 0.75 - adjustment)
+    ),
+    .init(
+      line:    CGPoint(x: 1.00, y: 0.30 + adjustment),
+      curve:   CGPoint(x: 0.95, y: 0.20 + adjustment),
+      control: CGPoint(x: 1.00, y: 0.25 + adjustment)
+    )
+  ]
+}
