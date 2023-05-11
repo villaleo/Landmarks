0693cdeaf340c5aea4fb71af558935124a8c0f02
Leo Villalobos <villaleobos@gmail.com>
2023-05-11 13:12:34 -0700

    Create CategoryItem.swift

---
+++ Landmarks/Views/CategoryItem.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by Leonardo Villalobos on 5/11/23.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+  var item: Landmark
+  var body: some View {
+    VStack(alignment: .leading) {
+      item.image
+        .resizable()
+        .frame(width: 155, height: 155)
+        .cornerRadius(5)
+      Text(item.name)
+        .font(.caption)
+    }
+    .padding(.leading, 15)
+  }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+  static var landmark = ModelData().landmarks.first!
+  static var previews: some View {
+    CategoryItem(item: landmark)
+  }
+}
