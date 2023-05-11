0749ac1c4cf05788d7b176b9d3b47faa3751ddbd
Leo Villalobos <villaleobos@gmail.com>
2023-05-11 13:12:54 -0700

    Create CategoryRow.swift

---
+++ Landmarks/Views/CategoryRow.swift
@@ -0,0 +1,41 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Leonardo Villalobos on 5/11/23.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+  var categoryName: String
+  var items: [Landmark]
+  
+  var body: some View {
+    VStack(alignment: .leading) {
+      Text(categoryName)
+        .font(.headline)
+        .padding(.leading, 15)
+        .padding(.top, 5)
+      
+      ScrollView(.horizontal, showsIndicators: false) {
+        HStack {
+          ForEach(items) { landmark in
+            CategoryItem(item: landmark)
+          }
+        }
+      }
+      .frame(height: 185)
+    }
+  }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+  static var landmarks = ModelData().landmarks
+  static var previews: some View {
+    CategoryRow(
+      categoryName: landmarks.first!.category.rawValue,
+      items: Array(landmarks.prefix(upTo: 4))
+    )
+  }
+}
