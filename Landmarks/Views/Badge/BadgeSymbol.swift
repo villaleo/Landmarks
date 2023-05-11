246d97643ffbaf35e5edb6113fd8c2906f750410
Leo Villalobos <villaleobos@gmail.com>
2023-05-11 08:44:16 -0700

    Create BadgeSymbol.swift

---
+++ Landmarks/Views/Badge/BadgeSymbol.swift
@@ -0,0 +1,51 @@
+//
+//  BadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Leonardo Villalobos on 5/11/23.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+      GeometryReader { geometry in
+        Path { path in
+          let width = min(geometry.size.width, geometry.size.height)
+          let height = width * 0.75
+          let spacing = width * 0.030
+          let middle = width * 0.5
+          let topWidth = width * 0.226
+          let topHeight = height * 0.488
+
+          // Draw the mountain peak
+          path.addLines([
+            CGPoint(x: middle, y: spacing),
+            CGPoint(x: middle - topWidth, y: topHeight - spacing),
+            CGPoint(x: middle, y: topHeight / 2 + spacing),
+            CGPoint(x: middle + topWidth, y: topHeight - spacing),
+            CGPoint(x: middle, y: spacing)
+          ])
+          
+          path.move(to: CGPoint(x: middle, y: topHeight / 2 + spacing * 3))
+          // Draw the mountain base
+          path.addLines([
+            CGPoint(x: middle - topWidth, y: topHeight + spacing),
+            CGPoint(x: spacing, y: height - spacing),
+            CGPoint(x: width - spacing, y: height - spacing),
+            CGPoint(x: middle + topWidth, y: topHeight + spacing),
+            CGPoint(x: middle, y: topHeight / 2 + spacing * 3)
+          ])
+        }
+        .fill(Self.symbolColor)
+      }
+    }
+  
+  static let symbolColor = Color(red: 90.0 / 255, green: 89.0 / 255, blue: 191.0 / 255)
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
