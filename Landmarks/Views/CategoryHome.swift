bfc9c792371ea081bb03825a62c2402b239c3439
Leo Villalobos <villaleobos@gmail.com>
2023-05-11 12:50:36 -0700

    Create CategoryHome.swift

---
+++ Landmarks/Views/CategoryHome.swift
@@ -0,0 +1,31 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Leonardo Villalobos on 5/11/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+  @EnvironmentObject var modelData: ModelData
+  
+  var body: some View {
+    NavigationStack {
+      List {
+        ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+          Text(key)
+        }
+      }
+        .navigationTitle("Featured")
+    }
+  }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+  static var modelData = ModelData()
+  static var previews: some View {
+    CategoryHome()
+      .environmentObject(modelData)
+  }
+}
