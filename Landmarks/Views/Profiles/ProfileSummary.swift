c97d9544a8121b585e377d20eeb2ea2f528978bf
Leo Villalobos <villaleobos@gmail.com>
2023-05-12 15:33:39 -0700

    Complete ProfileSummary.swift with default data

--- Landmarks/Views/Profiles/ProfileSummary.swift
+++ Landmarks/Views/Profiles/ProfileSummary.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ProfileSummary: View {
+  @EnvironmentObject var modelData: ModelData
+  
   var profile: Profile
   
   var body: some View {
@@ -19,6 +21,31 @@ struct ProfileSummary: View {
         Text("Notifications: \(profile.prefersNotifications ? "On" : "Off")")
         Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
         Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+        Divider()
+        
+        VStack(alignment: .leading) {
+          Text("Completed Badges")
+            .font(.headline)
+          
+          ScrollView(.horizontal) {
+            HStack {
+              HikeBadge(name: "First Hike")
+              HikeBadge(name: "Earth Day")
+                .hueRotation(Angle(degrees: -65))
+              HikeBadge(name: "Tenth Hike")
+                .grayscale(0.5)
+                .hueRotation(Angle(degrees: 100))
+            }
+            .padding(.bottom)
+          }
+        }
+        
+        Divider()
+        VStack(alignment: .leading) {
+          Text("Recent Hikes")
+            .font(.headline)
+          HikeView(hike: modelData.hikes.first!)
+        }
       }
     }
   }
@@ -26,7 +53,9 @@ struct ProfileSummary: View {
 
 struct ProfileSummary_Previews: PreviewProvider {
   static var profile = Profile.default
+  static var modelData = ModelData()
   static var previews: some View {
     ProfileSummary(profile: profile)
+      .environmentObject(modelData)
   }
 }
