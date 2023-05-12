abc885d729cd1062c29430af83aac9ad2af86b9d
Leo Villalobos <villaleobos@gmail.com>
2023-05-11 21:19:32 -0700

    Add Profile files

---
+++ Landmarks/Models/Profile.swift
@@ -0,0 +1,8 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Leonardo Villalobos on 5/11/23.
+//
+
+import Foundation
---
+++ Landmarks/Views/Profiles/ProfileSummary.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileSummary.swift
+//  Landmarks
+//
+//  Created by Leonardo Villalobos on 5/11/23.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileSummary_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSummary()
+    }
+}
