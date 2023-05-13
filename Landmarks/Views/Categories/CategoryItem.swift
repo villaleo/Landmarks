3a6726886950a3428c1a407d7ae6c2e35a706379
Leo Villalobos <villaleobos@gmail.com>
2023-05-12 21:17:14 -0700

    (CategoryItem.swift) Add a heart on featured thumbnails

--- Landmarks/Views/Categories/CategoryItem.swift
+++ Landmarks/Views/Categories/CategoryItem.swift
@@ -11,11 +11,23 @@ struct CategoryItem: View {
   var item: Landmark
   var body: some View {
     VStack(alignment: .leading) {
-      item.image
-        .renderingMode(.original)
-        .resizable()
-        .frame(width: 155, height: 155)
-        .cornerRadius(5)
+      ZStack {
+        item.image
+          .renderingMode(.original)
+          .resizable()
+          .frame(width: 155, height: 155)
+          .cornerRadius(5)
+          .padding(.top)
+        
+        if item.isFavorite {
+          Image(systemName: "heart.fill")
+            .resizable()
+            .frame(width: 20, height: 20)
+            .foregroundColor(.red)
+            .shadow(radius: 5)
+            .offset(x: 155/2, y: -(155/2) + 10)
+        }
+      }
       Text(item.name)
         .foregroundColor(.primary)
         .font(.caption)
@@ -25,7 +37,7 @@ struct CategoryItem: View {
 }
 
 struct CategoryItem_Previews: PreviewProvider {
-  static var landmark = ModelData().landmarks.first!
+  static var landmark = ModelData().landmarks[0]
   static var previews: some View {
     CategoryItem(item: landmark)
   }
