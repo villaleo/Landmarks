976ef883e445ae0b1d53aec4b5b7d3dd5d770cc5
Leo Villalobos <villaleobos@gmail.com>
2023-05-12 18:12:00 -0700

    Conform Coordinator to UIPageViewControllerDataSource

--- Landmarks/Views/Pages/PageViewController.swift
+++ Landmarks/Views/Pages/PageViewController.swift
@@ -10,14 +10,51 @@ import UIKit
 
 struct PageViewController<Page: View> {
   var pages: [Page]
+  
+  class Coordinator: NSObject, UIPageViewControllerDataSource {
+    var parent: PageViewController
+    var controllers = [UIViewController]()
+    
+    init(_ parent: PageViewController) {
+      self.parent = parent
+      self.controllers = parent.pages.map { UIHostingController(rootView: $0) }
+    }
+    
+    func pageViewController(
+      _ pageViewController: UIPageViewController,
+      viewControllerBefore viewController: UIViewController
+    ) -> UIViewController? {
+      guard let index = controllers.firstIndex(of: viewController) else {
+        return nil
+      }
+      
+      if index == 0 {
+        return controllers.last
+      }
+      return controllers[index - 1]
+    }
+    
+    func pageViewController(
+      _ pageViewController: UIPageViewController,
+      viewControllerAfter viewController: UIViewController
+    ) -> UIViewController? {
+      guard let index = controllers.firstIndex(of: viewController) else {
+        return nil
+      }
+      
+      if index == controllers.count - 1 {
+        return controllers.first
+      }
+      return controllers[index + 1]
+    }
+  }
 }
 
 extension PageViewController: UIViewControllerRepresentable {
   func makeUIViewController(context: Context) -> some UIViewController {
-    return UIPageViewController(
-      transitionStyle: .scroll,
-      navigationOrientation: .horizontal
-    )
+    let controller = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+    controller.dataSource = context.coordinator
+    return controller
   }
   
   func updateUIViewController(_ controller: UIViewControllerType, context: Context) {
@@ -25,10 +62,10 @@ extension PageViewController: UIViewControllerRepresentable {
       fatalError("Couldn't cast UIViewController to UIPageViewController")
     }
     
-    controller.setViewControllers(
-      [UIHostingController(rootView: pages.first!)],
-      direction: .forward,
-      animated: true
-    )
+    controller.setViewControllers([context.coordinator.controllers.first!], direction: .forward, animated: true)
+  }
+  
+  func makeCoordinator() -> Coordinator {
+    Coordinator(self)
   }
 }
